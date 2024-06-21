806fb12462129d74a2280084b82dd90fbbba6f71
Krysta Hunt <kdhunt@musefish.net>
2024-06-21 15:00:06 -0700

    all these files

---
+++ src/routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const authenticateJWT = require('../middleware/auth');
+
+const router = express.Router();
+
+// Example CRUD routes
+router.get('/items', authenticateJWT, (req, res) => {
+  // Get all items
+});
+
+router.post('/items', authenticateJWT, (req, res) => {
+  // Create a new item
+});
+
+router.put('/items/:id', authenticateJWT, (req, res) => {
+  // Update an item
+});
+
+router.delete('/items/:id', authenticateJWT, (req, res) => {
+  // Delete an item
+});
+
+module.exports = router;
+
